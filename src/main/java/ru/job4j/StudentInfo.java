aa8a359852f45b11bae3fa5f6bc7c2ee315def60
“Прокопенко <Deniss_2005@ukr.net>
2022-02-21 13:03:48 +0200

    3.1 Закгрузка файлов  #243562  Multiple

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Прокопенко Денис");
+        System.out.println("12.02.78");
+
+    }
+
+}
